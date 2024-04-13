bb807cc500dc44b4a6ab0a27d9f5b59df161d91c
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2024-04-13 09:43:26 -0400

    fixed harpoon and themes

--- lua/custom/harpoon.lua
+++ lua/custom/harpoon.lua
@@ -7,9 +7,9 @@ harpoon:setup {}
 vim.keymap.set('n', '<leader>a', function()
   harpoon:list():add()
 end)
--- vim.keymap.set('n', '<leader>e', function()
---   harpoon.ui:toggle_quick_menu(harpoon:list())
--- end)
+vim.keymap.set('n', '<leader>e', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
 
 vim.keymap.set('n', '<leader>1', function()
   harpoon:list():select(1)
@@ -31,35 +31,3 @@ end)
 vim.keymap.set('n', '<S-l>', function()
   harpoon:list():next()
 end)
-
--- basic telescope configuration
-local conf = require('telescope.config').values
-local function toggle_telescope(harpoon_files)
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
-
-  require('telescope.pickers')
-    .new({}, {
-      prompt_title = 'Harpoon',
-      finder = require('telescope.finders').new_table {
-        results = file_paths,
-      },
-      previewer = conf.file_previewer {},
-      sorter = conf.generic_sorter {},
-      attach_mappings = function(_, map)
-        map('i', '<c-d>', function(prompt_bufnr)
-          local selection = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
-          harpoon:list():remove_at(selection.index)
-          print('Deleted ' .. selection.value)
-        end)
-        return true
-      end,
-    })
-    :find()
-end
-
-vim.keymap.set('n', '<leader>e', function()
-  toggle_telescope(harpoon:list())
-end, { desc = 'Open harpoon window' })
