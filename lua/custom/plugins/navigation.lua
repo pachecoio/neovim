23843d9ee554e2595246762d1e3a87c7bc3ac582
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-02-10 14:56:59 -0300

    Included my preferred base plugins and custom options

---
+++ lua/custom/plugins/navigation.lua
@@ -0,0 +1,22 @@
+-- Move to left split with ctrl h
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left split' })
+-- Move to right split with ctrl l
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right split' })
+-- Move to top split with ctrl k
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to top split' })
+-- Move to bottom split with ctrl j
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to bottom split' })
+
+local map = vim.keymap.set
+
+map('n', ';', ':', { desc = 'CMD enter command mode' })
+map('i', 'jk', '<ESC>')
+
+-- map("n", "<Leader>m", ":NvimTreeToggle <cr>", { desc = "Open file tree" })
+
+map('n', '<Leader>q', ':q<cr>', { desc = 'Close buffer' })
+map('n', '<Leader>fs', ':w<cr>', { desc = 'Save file' })
+
+map('t', '<esc>', '<C-\\><C-n>')
+
+return {}
