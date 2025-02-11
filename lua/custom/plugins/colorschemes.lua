d07a685680eb4d106a118da0b0e2ca0ef854e131
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-02-10 22:28:37 -0300

    revert back to use tokyonight

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    'scottmckendry/cyberdream.nvim',
-    lazy = false,
-    config = function()
-      -- set colorscheme
-      vim.cmd.colorscheme 'cyberdream'
-    end,
-  },
+  -- {
+  --   'scottmckendry/cyberdream.nvim',
+  --   lazy = false,
+  --   config = function()
+  --     -- set colorscheme
+  --     vim.cmd.colorscheme 'cyberdream'
+  --   end,
+  -- },
   -- {
   --   'rebelot/kanagawa.nvim',
   --   lazy = false,
